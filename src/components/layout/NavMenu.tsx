aeeeba72f8d29cdde40577754fab49601528249f
mhammadsiddiqui <mh.siddiqui89@hotmail.com>
2021-10-24 10:51:12 +0200

    WIP: app name

--- src/components/layout/NavMenu.tsx
+++ src/components/layout/NavMenu.tsx
@@ -14,7 +14,7 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
                     <Container>
-                        <NavbarBrand tag={Link} to="/">ReactReduxApp</NavbarBrand>
+                        <NavbarBrand tag={Link} to="/"><h3>SIXT</h3></NavbarBrand>
                         <NavbarToggler onClick={this.toggle} className="mr-2" />
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
                             <ul className="navbar-nav flex-grow">
